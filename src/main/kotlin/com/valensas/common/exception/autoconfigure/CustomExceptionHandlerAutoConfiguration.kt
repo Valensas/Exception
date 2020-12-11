e66fdb8f05aa6ae2b2578b4a1baee0b8c54a5dc2
Can Yaman <can@yaman.me>
2020-12-11 18:44:29 +0300

    Feature: WebClient Response Exception propogation

--- src/main/kotlin/com/valensas/common/exception/autoconfigure/CustomExceptionHandlerAutoConfiguration.kt
+++ src/main/kotlin/com/valensas/common/exception/autoconfigure/CustomExceptionHandlerAutoConfiguration.kt
@@ -1,5 +1,6 @@
 package com.valensas.common.exception.autoconfigure
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import com.valensas.common.exception.CustomErrorAttributes
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -9,7 +10,7 @@ import org.springframework.context.annotation.Primary
 class CustomExceptionHandlerAutoConfiguration {
     @Bean
     @Primary
-    fun customErrorAttributes(): CustomErrorAttributes {
-        return CustomErrorAttributes()
+    fun customErrorAttributes(objectMapper: ObjectMapper): CustomErrorAttributes {
+        return CustomErrorAttributes(objectMapper)
     }
 }
